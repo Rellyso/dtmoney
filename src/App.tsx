9cbb0f35e20c4914601c849b8190c8f31a307c26
rellyso <rellysondouglas2015@gmail.com>
2021-03-18 23:21:47 -0300

    Added ContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Modal from 'react-modal';
 
 import { Dashboard } from "./components/Dashboard";
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext, TransactionsProvider } from "./TransactionsContext";
 
 
 Modal.setAppElement('#root')
@@ -22,13 +23,13 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
